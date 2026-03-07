8843b95a6a144ec132ff0acdf8857c78fa1add08
jun-ercia <l.ercia@lcba.edu.ph>
2026-03-07 16:34:33 +0800

    Update contact_application.py

--- contact_application.py
+++ contact_application.py
@@ -1,19 +1,25 @@
+"""
+File Name: contact_application.py
+Program: Contact Manager Application
+
+Author: Jun Y. Ercia
+
+Description:
+Defines the ContactApplication class which acts as the main
+controller of the system. It coordinates the interaction
+between the user interface (Menu) and the contact management
+logic (ContactManager).
+"""
+
 from contact import Contact
 from contact_manager import ContactManager
 from menu import Menu
 
 
-# =========================================================
-# Class: ContactApplication
-#
-# Description:
-#     Main controller coordinating menu and contact manager.
-# =========================================================
-
-
 class ContactApplication:
 
     def __init__(self):
+        """Initializes the menu and contact manager."""
 
         self.__menu = Menu([
             "Add Contact",
@@ -27,13 +33,18 @@ class ContactApplication:
         self.__manager = ContactManager()
 
     def run(self):
+        """
+        Executes the main program loop.
+
+        The system repeatedly displays the menu and performs
+        the selected operation until the user exits.
+        """
 
         while True:
 
             option = self.__menu.display()
 
             if option == 1:
-
                 name = input("Name: ")
                 phone = input("Phone: ")
                 email = input("Email: ")
@@ -42,28 +53,24 @@ class ContactApplication:
                 self.__manager.add_contact(contact)
 
             elif option == 2:
-
                 name = input("Contact to update: ")
                 self.__manager.update_contact(name)
 
             elif option == 3:
-
                 name = input("Contact to delete: ")
                 self.__manager.delete_contact(name)
 
             elif option == 4:
-
                 keyword = input("Search keyword: ")
                 results = self.__manager.search_contact(keyword)
 
                 for contact in results:
+                    print("----------------")
                     contact.display()
 
             elif option == 5:
-
                 self.__manager.view_contacts()
 
             elif option == 6:
-
                 print("Goodbye.")
                 break
